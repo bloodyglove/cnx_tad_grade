da0e08a1840b68073d796a79893089c46b91f499
wised <wised@sapphire.co.th>
2014-08-23 15:13:21 +0700

    edit index.php

--- src/index.php
+++ src/index.php
@@ -2,11 +2,19 @@
 <head>
 <?php
 	include "grade.php";
+	if(isset($_GET['score'])){
+		$grade = new Grade();
+		$textGrade = $grade->tad_All($_GET['score']);
+	}
 ?>
 </head>
 <body>
 <form id="inputGrade" name="inputGrade" method="get" action="#">
-	<input type="text" name="score" id="score" value="">
+	<input type="text" name="score" id="score" value="<?php echo $_GET['score'];?>">
 	<input type="submit" value="submit">
+<?php 
+	echo '<hr>'.$textGrade.'<hr>';
+?>
 </form>
+
 </body>
