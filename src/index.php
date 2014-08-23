7cbf8749e8c4a97b99d6c8eece13e7d55e71a07a
wised <wised@sapphire.co.th>
2014-08-23 15:08:03 +0700

    add index.php

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<?php
+	include "grade.php";
+?>
+</head>
+<body>
+<form id="inputGrade" name="inputGrade" method="get" action="#">
+	<input type="text" name="score" id="score" value="">
+	<input type="submit" value="submit">
+</form>
+</body>
